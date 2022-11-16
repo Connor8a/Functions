3f87fb96cc8fea0593bd9b469134676b07005666
Connor8a <connorochoa@mail.weber.edu>
2022-11-15 18:58:26 -0700

    Get User Number

---
+++ functions.py
@@ -0,0 +1,19 @@
+# Conversion Calculator
+# By: Connor Ochoa 
+
+# Conversion of Length 
+Length = int(input("Please enter a number: "))
+print(Length)
+
+# User input regarding the length to convert 
+
+
+# Get the unit 
+
+#convert the length to the correct unit 
+
+# convert the length 
+
+# Output the answer
+
+# Graceful Conclusion 

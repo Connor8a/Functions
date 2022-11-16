6a3836f7fd60173e9b9f89574508f0aadf10edb4
Connor8a <connorochoa@mail.weber.edu>
2022-11-15 19:04:50 -0700

    Convert

--- functions.py
+++ functions.py
@@ -8,6 +8,14 @@ print(Length)
 # User input regarding the length to convert 
 user_unit = input("What unit of measurement is your number (ex: inches): ")
 
+#to convert inches to milmeters --> in x 25.4
+#to convert milmeters to inches --> mm / 25.4
+
+#user gives in unit 
+conv_number = Length * 25.4
+
+print(conv_number)
+print(user_unit)
 
 # Get the unit 
 

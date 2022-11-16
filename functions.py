08bc1343dbedd9a0a8c18c0c88c865809fe14a1c
Connor8a <connorochoa@mail.weber.edu>
2022-11-15 19:14:55 -0700

    force input and converted units

--- functions.py
+++ functions.py
@@ -6,15 +6,32 @@ Length = int(input("Please enter a number: "))
 print(Length)
 
 # User input regarding the length to convert 
-user_unit = input("What unit of measurement is your number (ex: inches): ")
+while (True):
+    try:
+     user_unit = input("What unit of measurement is your number (ex: inches): ")
+    except:
+        print("invalid input")
+    else:
+        if user_unit != "inches" or "milimeters":
+            print("invalid input")
+        else:
+            break
 
-#to convert inches to milmeters --> in x 25.4
-#to convert milmeters to inches --> mm / 25.4
-
+#to convert inches to milimeters --> in x 25.4
+#to convert milimeters to inches --> mm / 25.4
 #user gives in unit 
-conv_number = Length * 25.4
+if user_unit == "inches":
+    conv_number = Length * 25.4
+    print(conv_number)
+    conv_unit = 'mm'
 
-print(conv_number)
+if user_unit == "milimeters":
+    conv_number2 = Length / 25.4
+    print(conv_number2)
+    conv_unit = 'in'
+
+
+
 print(user_unit)
 
 # Get the unit 

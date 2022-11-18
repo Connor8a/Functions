f2b99807f90fe95fa6dd5bcbd63dc29018093959
Connor8a <connorochoa@mail.weber.edu>
2022-11-17 18:55:13 -0700

    changes

--- functions.py
+++ functions.py
@@ -2,7 +2,27 @@
 # By: Connor Ochoa 
 
 # Conversion of Length 
-Length = int(input("Please enter a number: "))
+def user_parcer(Length, user_unit):
+    print('I will parse your data')
+    # figure out a way to seperate the number from unit
+    values = Length.rsplit(" ")
+    print (values)
+
+
+
+while(True):
+    try:
+        Length = float(input("Please enter a number: "))
+        # Length = input("number and unit to convert: ")
+        #if Length.isdigit():
+        #   user_number = float (user_number)
+        #   break
+        #else:
+        #   print("please use a number")
+    except:
+        print("invalid input")
+    else:
+        break
 print(Length)
 
 # User input regarding the length to convert 
@@ -12,10 +32,10 @@ while (True):
     except:
         print("invalid input")
     else:
-        if user_unit != "inches" or user_unit != "milimeters":
-            print("invalid input")
-        else:
+        if user_unit == "inches" or user_unit == "milimeters":
             break
+        else:
+            print("invalid input")
 
 #to convert inches to milimeters --> in x 25.4
 #to convert milimeters to inches --> mm / 25.4
@@ -23,13 +43,12 @@ while (True):
 if user_unit == "inches":
     conv_number = Length * 25.4
     print(conv_number)
-    conv_unit = 'mm'
+    conv_unit = 'in'
 
 if user_unit == "milimeters":
     conv_number2 = Length / 25.4
     print(conv_number2)
-    conv_unit = 'in'
-
+    conv_unit = 'mm'
 
 
 
